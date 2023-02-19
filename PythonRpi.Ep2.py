cd59529e4a09c2c27b316386fabfa34e58a6f172
Natchaphol Janwiruch <125615994+NOIDxx@users.noreply.github.com>
2023-02-19 09:56:09 +0700

    Add files via upload

---
+++ PythonRpi.Ep2.py
@@ -0,0 +1,44 @@
+print ('Hello World')
+
+name = 'NOID'
+lastname = 'PowerUp'
+
+print(name)
+print(lastname)
+
+fullname = name +' '+ lastname
+
+print(fullname)
+
+Temp = '25.5'
+
+print('อุณหภูมิ '+ Temp + ' องศาเซลเซียส')
+
+print('อุณหภูมิ'+' '+Temp+' '+'องศาเซลเซียส')
+
+print ('อุณหภูมิ',Temp,'องศาเซลเซียส')
+
+print('อุณหภูมิ {} องศาเซลเซียส'.format(Temp))
+
+print(f'อุณหภูมิ {Temp} องศาเซลเซียส')
+
+print(type(Temp))
+
+print(float(Temp)*10)
+
+print(float(Temp))
+
+Temp2 = 25.5
+print (Temp2)
+print(type(Temp2))
+
+print(f'อุณหูภูมิ {Temp2:.2f} องศาเซลเซียส')
+
+money = 123456789.5046879
+
+print('มีเงิน {:,.2f} บาท'.format(money))
+
+print(f'มีเงิน {money:,.2f} บาท')
+
+
+
